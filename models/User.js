f9a7edf82547af576acf1b5b610fb0724b5f70f4
Suresh5295 <dinu05021995@gmail.com>
2024-11-20 21:11:37 +0530

    Initial commit_v6

--- models/User.js
+++ models/User.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    status: { type: String, enum: ['Pending', 'Approved', 'Activated'], default: 'Pending' },
-    activationDate: { type: Date },
-    expiryDate: { type: Date },
-}, { timestamps: true });
+    phone: { type: String, default: null }, // Optional field
+    fullname: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    company: { type: String, required: true },
+    country: { type: String, required: true },
+    description: { type: String, default: '' } // Optional field
+});
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', UserSchema);
